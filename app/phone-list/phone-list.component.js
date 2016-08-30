766e315fb7b4a782e79ec8bd7b9d044e4925076c
dbconsulting <buxton_dave_n@smc.edu>
2016-08-30 15:41:59 -0700

    added inline annotation to controller for '$http' service

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -1,17 +1,18 @@
+/*jslint white: true */
 'use strict';
 
 // Register `phoneList` component, along with its associated controller and template
-angular.
-  module('phoneList').
-  component('phoneList', {
-    templateUrl: 'phone-list/phone-list.template.html',
-    controller: function PhoneListController($http) {
+angular.module('phoneList').component('phoneList', {
+  templateUrl: 'phone-list/phone-list.template.html',
+  controller: ['$http',
+    function PhoneListController($http) {
       var self = this;
       self.orderProp = 'age';
 
-      $http.get('phones/phones.json').then(function(response) {
+      $http.get('phones/phones.json').then(function (response) {
         //self.phones = response.data;
-		self.phones = response.data.slice(0, 5); // limit to first 5 phones
+        self.phones = response.data.slice(0, 5); // limit to first 5 phones
       });
     }
-  });
+  ]
+});
